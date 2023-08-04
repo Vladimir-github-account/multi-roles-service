684da18dbf8211a3d2ce395d9a97f5e50256f9a9
volodymyr <volodymyr.dashkevychit@gmail.com>
2023-08-04 13:33:35 +0300

    create: files module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,10 +4,11 @@ import { PostsController } from './posts.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from '../roles/roles.model';
 import { Post } from './posts.model';
+import { FilesModule } from '../files/files.module';
 
 @Module({
   providers: [PostsService],
   controllers: [PostsController],
-  imports: [SequelizeModule.forFeature([Role, Post])],
+  imports: [SequelizeModule.forFeature([Role, Post]), FilesModule],
 })
 export class PostsModule {}
